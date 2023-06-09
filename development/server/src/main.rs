b6af0aad18a17a53ebed5273b11500c1ceb7c069
fbruggem <fbruggem@student.42heilbronn.de>
2023-06-09 16:00:08 +0200

    d

--- development/server/src/main.rs
+++ development/server/src/main.rs
@@ -1,18 +1,28 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-// Define your API endpoint handler
-#[get("/api/hello")]
+#[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello from Rust API!")
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Start the Actix web server
     HttpServer::new(|| {
-        App::new().service(hello)
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
     })
-    .bind("127.0.0.1:8000")? // Specify the IP address and port
+    .bind(("127.0.0.1", 8081))?
     .run()
     .await
 }
