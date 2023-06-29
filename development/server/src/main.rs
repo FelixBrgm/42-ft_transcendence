4bfeaec667a0de7ed055016931893428d0dbca18
FelixBrgm <github.badly321@passinbox.com>
2023-06-30 00:15:23 +0200

    works async

--- development/server/src/main.rs
+++ development/server/src/main.rs
@@ -1,4 +1,17 @@
-use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener};
+use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener, net::TcpStream};
+
+async fn process(mut socket: TcpStream) {
+    loop {
+        let mut buf: [u8; 512] = [0; 512];
+
+        let read_bytes: usize = socket.read(&mut buf).await.unwrap();
+        if read_bytes != 0 {
+            println!("{}", String::from_utf8_lossy(&buf));
+            socket.write("Got: ".as_bytes()).await.unwrap();
+            socket.write(&buf).await.unwrap();
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -6,16 +19,8 @@ async fn main() {
 
     loop {
         let (mut socket, _) = listener.accept().await.unwrap();
-        loop {
-            let mut buf: [u8; 512] = [0; 512];
-
-            let read_bytes: usize = socket.read(&mut buf).await.unwrap();
-            if read_bytes != 0 {
-                println!("{}", String::from_utf8_lossy(&buf));
-                socket.write("Got: ".as_bytes()).await.unwrap();
-                socket.write(&buf).await.unwrap();
-            }
-        }
-        // socket.
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
