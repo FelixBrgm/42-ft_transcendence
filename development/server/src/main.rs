49eca4334dcdf4fca1cd56e83c756b8fc9c648b3
Felix Brüggemann <57171559+FelixBrgm@users.noreply.github.com>
2023-06-30 00:41:12 +0200

    Between solutinos

--- development/server/src/main.rs
+++ development/server/src/main.rs
@@ -1,26 +1,42 @@
+use std::sync::{Arc, Mutex};
 use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener, net::TcpStream};
 
-async fn process(mut socket: TcpStream) {
+// type Chat = Arc<Mutex<[String]>>;
+type Clients = Arc<Mutex<Vec<TcpStream>>>;
+
+async fn process(mut socket: TcpStream, clients: Clients) {
     loop {
         let mut buf: [u8; 512] = [0; 512];
 
         let read_bytes: usize = socket.read(&mut buf).await.unwrap();
         if read_bytes != 0 {
             println!("{}", String::from_utf8_lossy(&buf));
-            socket.write("Got: ".as_bytes()).await.unwrap();
-            socket.write(&buf).await.unwrap();
+            let mut clients = clients.lock().unwrap();
+            for client in clients.iter_mut() {
+                client.write("Message: ".as_bytes()).await.unwrap();
+                client.write(&buf).await.unwrap();
+            }
         }
     }
 }
 
 #[tokio::main]
 async fn main() {
+    // let chat: Chat = Arc::new(Mutex::new([]));
+    let clients: Clients = Arc::new(Mutex::new(Vec::new()));
+
     let listener = TcpListener::bind("127.0.0.1:4242").await.unwrap();
 
     loop {
         let (mut socket, _) = listener.accept().await.unwrap();
+
+        // let chat = chat.clone();
+        let clients = clients.clone();
+        let mut value = clients.lock().unwrap();
+        value.push(socket);
+
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, clients).await;
         });
     }
 }
