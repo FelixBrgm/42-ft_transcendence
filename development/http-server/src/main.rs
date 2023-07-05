b38a35e47280ed43c1489a78c09130e20478d5ff
FelixBrgm <github.badly321@passinbox.com>
2023-07-06 00:55:18 +0200

    added create table

--- development/http-server/src/main.rs
+++ development/http-server/src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use bb8::Pool;
 use bb8_postgres::tokio_postgres::NoTls;
 use bb8_postgres::PostgresConnectionManager;
+// use tokio_postgres::connect;
 
 type DataBasePool = Pool<PostgresConnectionManager<NoTls>>;
 type DataBaseConnection<'a> = bb8::PooledConnection<'a, PostgresConnectionManager<NoTls>>;
@@ -34,6 +35,25 @@ async fn create_pool() -> Result<DataBasePool, Box<dyn std::error::Error>>
     Ok(pool)
 }
 
+async fn create_table(pool: &DataBasePool, name: &str) -> Result<(), Box<dyn std::error::Error>>
+{
+	let	connection: DataBaseConnection = pool.get().await?;
+
+	// Create a connection string
+    let create_string = format!(
+        "CREATE TABLE IF NOT EXISTS {} (id SERIAL PRIMARY KEY, name TEXT NOT NULL, age INT NOT NULL)",
+        name
+    );
+
+	connection
+	.execute(&create_string, &[])
+	.await?;
+
+	// Release the connection back to the pool
+	drop(connection);
+
+	Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>
@@ -41,14 +61,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>
 	// get_pool
 	let	pool: DataBasePool = create_pool().await?;
 
-    // Acquire a connection from the pool
-    let	connection: DataBaseConnection = pool.get().await?;
+	create_table(&pool, "clients").await?;
+	create_table(&pool, "others").await?;
+
+	let	connection: DataBaseConnection = pool.get().await?;
 
     // Spawn a task to process the connection in the background
     tokio::spawn(async move {
-        let _ = connection;
+		let _ = connection;
     });
-
+	
     println!("Connected to the database and acquired a connection");
 
     Ok(())
