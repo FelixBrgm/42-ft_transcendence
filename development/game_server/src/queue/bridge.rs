25d07415d87750f7757b941c84559b7b5f74f0f4
FelixBrgm <github.badly321@passinbox.com>
2023-07-08 21:19:39 +0200

    a

---
+++ development/game_server/src/queue/bridge.rs
@@ -0,0 +1,55 @@
+use futures_util::{SinkExt, StreamExt};
+use tokio::{
+    net::TcpStream,
+    sync::mpsc::{self, Receiver, Sender},
+};
+use tokio_tungstenite::WebSocketStream;
+use tungstenite::Message;
+
+pub(crate) fn bridge(
+    socket: WebSocketStream<TcpStream>,
+) -> (Sender<String>, Receiver<String>, Receiver<()>) {
+    let (mscp_to_socket_sender, mut mscp_to_socket_receiver) = mpsc::channel::<String>(1);
+    let (socket_to_mscp_sender, socket_to_mscp_receiver) = mpsc::channel::<String>(1);
+    let (disconnect_sender, disconnect_receiver) = mpsc::channel::<()>(1);
+
+    let (mut write, mut read) = socket.split();
+
+    tokio::spawn(async move {
+        loop {
+            match read.next().await {
+                Some(msg) => {
+                    if let Ok(msg) = msg {
+                        if let Ok(msg) = msg.into_text() {
+                            let _ = socket_to_mscp_sender.send(msg).await;
+                        };
+                    }
+                }
+                None => {
+                    let _ = disconnect_sender.send(()).await;
+                    break;
+                } // Socket_to_mpsc_sender is dropped here that causes the channel to return None at the receiver
+            }
+        }
+    });
+    tokio::spawn(async move {
+        loop {
+            match mscp_to_socket_receiver.recv().await {
+                Some(msg) => {
+                    let _ = write.send(Message::Text(msg + "\n")).await;
+                    let _ = write.flush().await;
+                }
+                None => {
+                    let _ = write.close().await;
+                    break;
+                }
+            }
+        }
+    });
+
+    return (
+        mscp_to_socket_sender,
+        socket_to_mscp_receiver,
+        disconnect_receiver,
+    );
+}
---
+++ development/game_server/src/queue/game/utils.rs
@@ -0,0 +1,10 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub(crate) fn get_ms() -> u128 {
+    let now = SystemTime::now();
+    let duration = now
+        .duration_since(UNIX_EPOCH)
+        .expect("Failed to calculate duration");
+    let milliseconds = duration.as_secs() as u128 * 1000 + u128::from(duration.subsec_millis());
+    milliseconds
+}
