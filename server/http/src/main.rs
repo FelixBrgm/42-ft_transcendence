fe739055069377b191fcc40bfd84b659324a7c08
Anna Fenzl <annafenzl@mymac.local>
2023-07-10 16:03:40 +0200

    with loading url

--- server/http/src/main.rs
+++ server/http/src/main.rs
@@ -1,24 +1,31 @@
+use std::path::Path;
+use dotenvy::dotenv;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use diesel::pg::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
 
+type DbPool = Pool<ConnectionManager<PgConnection>>;
 
-#[actix_web::main]
-async fn main() ->  std::io::Result<()> {
-    dotenvy::dotenv().ok();
+fn create_db_pool() -> Result<DbPool, Box<dyn std::error::Error>> {
+
+	// retrieve the database url
+    dotenvy::from_path(Path::new("../../database/.env"))?;
+    dotenv().ok();
+    let database_url =  dotenvy::var("POSTGRES_URL")?;
 
     // Create a connection manager
-    let database_url = "postgres://theOperator:IHaveAllThePower@localhost/postgres_db";
     let manager = ConnectionManager::<PgConnection>::new(database_url);
 
-	println!("1");
-	
     // Create a connection pool
-    let pool = Pool::builder()
-	.build(manager)
-	.expect("Failed to create database pool");
-	
-	println!("1");
+	Ok(Pool::builder().build(manager)?)
+}
+
+#[actix_web::main]
+async fn main() ->  Result<(), Box<dyn std::error::Error>> {
+
+	let pool: DbPool = create_db_pool()?;
+
+
 	Ok(())
 
 }
