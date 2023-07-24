8606220cb8dd80d5a092f033cea164eaddf41f25
FelixBrgm <github.badly321@passinbox.com>
2023-07-25 00:15:25 +0200

    added cors, values not sure yet

--- source/server/http/src/api/errors.rs
+++ source/server/http/src/api/errors.rs
@@ -4,27 +4,27 @@ use derive_more::Display;
 
 #[derive(Debug, Display)]
 pub enum ApiError {
-    #[display(fmt = "Bad Request: {}", _0)]
-    BadRequest(String),
-    #[display(fmt = "Unauthorized")]
+	#[display(fmt = "Unauthorized")]
     Unauthorized,
     #[display(fmt = "Not Found")]
     NotFound,
-    #[display(fmt = "Internal Server Error")]
-    InternalServerError,
+    #[display(fmt = "Bad Request: {}", _0)]
+    BadRequest(String),
+    #[display(fmt = "Internal Server Error: {}", _0)]
+    InternalServerError(String),
 }
 
 // Implement ResponseError trait for ApiError to return a Http response
 impl ResponseError for ApiError {
     fn error_response(&self) -> HttpResponse {
         match self {
-            ApiError::BadRequest(message) => {
-                HttpResponse::BadRequest().body(message.to_string())
-            }
             ApiError::Unauthorized => HttpResponse::Unauthorized().body("Unauthorized"),
             ApiError::NotFound => HttpResponse::NotFound().body("Not Found"),
-            ApiError::InternalServerError => {
-                HttpResponse::InternalServerError().body("Internal Server Error")
+			ApiError::BadRequest(message) => {
+                HttpResponse::BadRequest().body(message.to_string())
+            }
+            ApiError::InternalServerError(message) => {
+                HttpResponse::InternalServerError().body(message.to_string())
             }
         }
     }
