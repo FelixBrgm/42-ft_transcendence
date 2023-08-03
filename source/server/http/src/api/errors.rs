0d57a49251bbc8e952c8cdc938db667b51c22773
Anna Fenzl <annafenzl@mymac.local>
2023-08-03 21:45:49 +0200

    nicer user get

--- source/server/http/src/api/errors.rs
+++ source/server/http/src/api/errors.rs
@@ -1,36 +1,5 @@
-// use actix_web::error::ResponseError;
-// use actix_web::HttpResponse;
-// use derive_more::Display;
-
-// #[derive(Debug, Display)]
-// pub enum ApiError {
-// 	#[display(fmt = "Unauthorized")]
-//     Unauthorized,
-//     #[display(fmt = "Not Found")]
-//     NotFound,
-//     #[display(fmt = "Bad Request: {}", _0)]
-//     BadRequest(String),
-//     #[display(fmt = "Internal Server Error: {}", _0)]
-//     InternalServerError(String),
-// }
-
-// // Implement ResponseError trait for ApiError to return a Http response
-// impl ResponseError for ApiError {
-//     fn error_response(&self) -> HttpResponse {
-//         match self {
-//             ApiError::Unauthorized => HttpResponse::Unauthorized().body("Unauthorized"),
-//             ApiError::NotFound => HttpResponse::NotFound().body("Not Found"),
-// 			ApiError::BadRequest(message) => {
-//                 HttpResponse::BadRequest().body(message.to_string())
-//             }
-//             ApiError::InternalServerError(message) => {
-//                 HttpResponse::InternalServerError().body(message.to_string())
-//             }
-//         }
-//     }
-// }
-
 use std::any;
+use std::num::ParseIntError;
 
 use actix_session::{SessionGetError, SessionInsertError};
 use actix_web::{HttpResponse, ResponseError};
@@ -80,8 +49,8 @@ impl From<SessionInsertError> for ApiError {
     }
 }
 
-impl From<reqwest::Error> for ApiError {
-    fn from(_: reqwest::Error) -> Self {
+impl From<ParseIntError> for ApiError {
+    fn from(_err: ParseIntError) -> Self {
         ApiError::InternalServerError
     }
 }
--- source/server/http/src/api/user.rs
+++ source/server/http/src/api/user.rs
@@ -9,25 +9,38 @@ pub fn init(cfg: &mut web::ServiceConfig)
 {
 	cfg.service(
 		web::scope("/user")
-		 .route("", web::get().to(get_user))
-		//  .route("", web::post().to());
+		 .route("", web::get().to(user_get))
+		//  .route("", web::post().to(user_put))
+		//  .route("/{}", web::get().to(user_id_get))
 	);
 }
 
-async fn get_user(identity: Identity, db: web::Data<Database>) -> Result<HttpResponse, ApiError> {
+// returns the information of the user sending the request
+async fn user_get(identity: Identity, db: web::Data<Database>) -> Result<HttpResponse, ApiError> {
 	println!("GET /api/user");
-	if let Ok(id_str) = identity.id()
-	{
-		println!("id is {}", id_str);
-		if let Ok(id) = id_str.parse::<i32>() {
-            let user = db.get_user_by_id(id);
-			if let Ok(user) = user{
-				println!("user is {:?}", user);
-				return Ok(HttpResponse::Ok().json(&user));
-			}
-        }
-		return Err(ApiError::InternalServerError);
-	}
-	return Err(ApiError::Unauthorized)
+
+	let Ok(id) = identity.id() else {
+		return Err(ApiError::Unauthorized)
+	};
+
+	let user = db.get_user_by_id(id.parse::<i32>()?)?;
+	Ok(HttpResponse::Ok().json(&user))
 }
+
+// // updates the information of the user sending the request
+// async fn user_put(identity: Identity, db: web::Data<Database>) -> Result<HttpResponse, ApiError> {
+// 	println!("PUT /api/user");
+// 	if let Ok(id_str) = identity.id()
+// 	{
+// 		if let Ok(id) = id_str.parse::<i32>() {
+//             let user = db.get_user_by_id(id);
+// 			if let Ok(user) = user{
+// 				println!("user is {:?}", user);
+// 				return Ok(HttpResponse::Ok().json(&user));
+// 			}
+//         }
+// 		return Err(ApiError::InternalServerError);
+// 	}
+// 	return Err(ApiError::Unauthorized)
+// }
 
