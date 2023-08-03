0b9adeb62fe38f80448c657502651b25618db361
FelixBrgm <github.badly321@passinbox.com>
2023-08-03 22:54:55 +0200

    checkpoint

--- source/server/http/src/api/user.rs
+++ source/server/http/src/api/user.rs
@@ -10,14 +10,13 @@ pub fn init(cfg: &mut web::ServiceConfig)
 	cfg.service(
 		web::scope("/user")
 		 .route("", web::get().to(user_get))
-		//  .route("", web::post().to(user_put))
-		//  .route("/{}", web::get().to(user_id_get))
+		 .route("", web::put().to(user_put))
+		 
 	);
 }
 
 // returns the information of the user sending the request
 async fn user_get(identity: Identity, db: web::Data<Database>) -> Result<HttpResponse, ApiError> {
-	println!("GET /api/user");
 
 	let Ok(id) = identity.id() else {
 		return Err(ApiError::Unauthorized)
@@ -25,6 +24,22 @@ async fn user_get(identity: Identity, db: web::Data<Database>) -> Result<HttpRes
 
 	let user = db.get_user_by_id(id.parse::<i32>()?)?;
 	Ok(HttpResponse::Ok().json(&user))
+}
+
+async fn user_put(identity: Identity, db: web::Data<Database>) -> Result<HttpResponse, ApiError> {
+	
+	let Ok(id) = identity.id() else {
+		return Err(ApiError::Unauthorized)
+	};
+
+	// let result = db.set_user();
+
+    // match result {
+    //     Ok(_) => Ok(HttpResponse::Ok().finish()),
+    //     Err(_) => Err(ApiError::InternalServerError),
+    // }
+
+	Ok(HttpResponse::Ok().finish())
 }
 
 // // updates the information of the user sending the request
