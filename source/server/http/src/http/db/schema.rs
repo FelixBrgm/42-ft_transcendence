b78437356ba98a7e31ab7abaf8361ad36c2fb279
Anna Fenzl <annafenzl@mymac.local>
2023-08-11 15:39:20 +0200

    schema.rs in right dir

--- source/server/http/src/http/db/schema.rs
+++ source/server/http/src/http/db/schema.rs
@@ -16,14 +16,43 @@ diesel::table! {
 }
 
 diesel::table! {
-    clients (id) {
+    chat_messages (id) {
         id -> Int4,
-        title -> Text,
-        is_online -> Bool,
+        room_id -> Int4,
+        sender_id -> Int4,
+        message -> Text,
+        timestamp -> Timestamp,
     }
 }
 
+diesel::table! {
+    chat_rooms (id) {
+        id -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+        #[max_length = 255]
+        topic -> Nullable<Varchar>,
+        is_public -> Bool,
+        password -> Nullable<Bytea>,
+    }
+}
+
+diesel::table! {
+    user_chat_room (id) {
+        id -> Int4,
+        user_id -> Int4,
+        room_id -> Int4,
+    }
+}
+
+diesel::joinable!(chat_messages -> app_user (sender_id));
+diesel::joinable!(chat_messages -> chat_rooms (room_id));
+diesel::joinable!(user_chat_room -> app_user (user_id));
+diesel::joinable!(user_chat_room -> chat_rooms (room_id));
+
 diesel::allow_tables_to_appear_in_same_query!(
     app_user,
-    clients,
+    chat_messages,
+    chat_rooms,
+    user_chat_room,
 );
