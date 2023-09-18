2268009e83c9ba2a43d2a872ec692f98687a0fcb
FelixBrgm <github.badly321@passinbox.com>
2023-09-18 15:02:03 +0200

    rm old http

--- source/server/http/migrations/2023-08-11-164422_room_user_connection/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE room_user_connection (
-    id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    room_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES app_user (id),
-    FOREIGN KEY (room_id) REFERENCES chat_rooms (id),
-    UNIQUE (user_id, room_id)
-);
