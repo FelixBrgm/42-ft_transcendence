56fca5e57501042427bfd5c592d4736e7f4bd8a9
FelixBrgm <github.badly321@passinbox.com>
2023-12-25 22:10:01 +0100

    chatserver setup without logic

--- source/server/http/src/api/ws.rs
+++ source/server/http/src/api/ws.rs
@@ -1,38 +1,69 @@
 
 use actix_identity::Identity;
 use actix_web_actors::ws::{Message, WebsocketContext};
+use actix::prelude::*;
 use super::error::ApiError;
 
-use actix::{Actor, StreamHandler};
+use crate::chat::{ChatServer, ClientMessage};
+use actix::{Actor, StreamHandler, Addr};
 use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
 
-// url -i -N -H "Connection: Upgrade" -H "Upgrade: websocket" -H "Sec-WebSocket-Version: 13" -H "Sec-WebSocket-Key: $(head -c 16 /dev/urandom | base64)" http://localhost:8080/ws
-
-struct MyWs;
-
-impl Actor for MyWs {
-    type Context = ws::WebsocketContext<Self>;
+struct ChatSession {
+	id: usize,
+	addr: Addr<ChatServer>,
 }
 
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-		match msg {
-            Ok(Message::Ping(ping)) => ctx.pong(&ping),
-            Ok(Message::Text(text)) => {
-                println!("Received message: {}", text);
-                ctx.text(format!("Echo: {}", text));
-            }
-            Ok(Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
-        }
+impl Actor for ChatSession {
+    type Context = ws::WebsocketContext<Self>;
+
+	fn started(&mut self, context: &mut Self::Context) {
+
+		// start heartbeat if i want to implement this
+
+		// let addr = context.address();
+		// self.addr.send(<ChatServer as Handler>::Connect {
+		// 	addr: addr.recipient(),
+		// })
+		// .into_actor(self)
+		// .then(|res, act, ctx| {
+		// 	match res {
+		// 		Ok(res) => act.id = res,
+		// 		// something is wrong
+		// 		_ => context.stop(),
+		// 	}
+		// 	fut::ready(())
+		// })
+		// .wait(context);
+	}
+
+	// stopping
+}
+
+impl Handler<ClientMessage> for ChatSession {
+    type Result = ();
+
+    fn handle(&mut self, msg: ClientMessage, ctx: &mut Self::Context) {
+		println!("chatsession message handler");
+        // ctx.text(msg.0);
     }
 }
 
+
+/// WebSocket message handler
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ChatSession {
+	fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+		println!("streamhandler of chatsession not implememted yet");
+	}
+}
+
+
 #[get("/ws")]
-async fn server(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, ApiError> {
-	let resp = ws::start(MyWs {}, &req, stream);
+async fn server(req: HttpRequest, stream: web::Payload, server: web::Data<Addr<ChatServer>>) -> Result<HttpResponse, ApiError> {
+	let resp = ws::start(ChatSession{
+		id: 0,
+		addr: server.get_ref().clone(),
+	}, &req, stream);
     println!("{:?}", resp);
    Ok(HttpResponse::Ok().body("test"))
 }
