91f892512fcc0cc9bb1cf7144afe3751c6a961b8
FelixBrgm <github.badly321@passinbox.com>
2024-01-11 13:56:06 +0100

    Added CountDown and more game logic

--- source/server/http/src/game/pong/mod.rs
+++ source/server/http/src/game/pong/mod.rs
@@ -5,6 +5,7 @@ mod player;
 
 use actix::prelude::*;
 use std::time::{Duration, Instant};
+use std::sync::{Arc, Mutex};
 
 pub use self::ball::Ball;
 pub use self::config::GameConfig;
@@ -32,6 +33,14 @@ pub struct UpdateScore {
 
 #[derive(Message)]
 #[rtype(result = "()")]
+pub struct CountDown;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct GameStart;
+
+#[derive(Message)]
+#[rtype(result = "()")]
 pub struct GameOver;
 
 #[derive(Debug, Clone)]
@@ -40,8 +49,8 @@ pub struct Pong {
     score: [u8; 2],
     ball: Ball,
     config: GameConfig,
-    last_tick_time: Instant,
     finished: bool,
+	paused: bool,
 }
 
 impl Pong {
@@ -51,8 +60,8 @@ impl Pong {
             score: [0; 2],
             ball: Ball::new(),
             config: GameConfig::new(),
-            last_tick_time: Instant::now(),
             finished: false,
+			paused: true,
         }
     }
 
@@ -72,15 +81,15 @@ impl Pong {
 
     fn update(&mut self, ctx: &mut Context<Self>) {
         self.ball
-            .update(100, &self.config, &self.players, &mut self.score, ctx);
+            .update(10, &self.config, &mut self.players, &mut self.score, ctx);
 
         for player in self.players.iter_mut() {
-            player.update(100, &self.config);
+            player.update(10, &self.config);
         }
     }
 
     fn tick(&mut self, ctx: &mut Context<Self>) {
-        if self.finished {
+        if self.finished || self.paused {
             return;
         }
 
@@ -97,12 +106,15 @@ impl Actor for Pong {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        self.send_to_players(Message("FORMAT: {PLAYER.0} {PLAYER.1} {BALL.x} {BALL.y}".to_owned()));
-		self.players[0].socket.do_send(Message("YOU : OTHER".to_owned()));
-		self.players[1].socket.do_send(Message("OTHER : YOU".to_owned()));
-        self.send_to_players(Message("BEG".to_owned()));
+
+		self.players[0].socket.do_send(Message("FORMAT: {YOU} {OTHER} {BALL.x} {BALL.y}".to_owned()));
+		self.players[1].socket.do_send(Message("FORMAT: {OTHER} {YOU} {BALL.x} {BALL.y}".to_owned()));
+
         self.ball.reset(&self.config);
-        self.tick(ctx);
+		self.players[0].reset(&self.config);
+		self.players[1].reset(&self.config);
+
+        ctx.notify(CountDown);
     }
 }
 
@@ -110,9 +122,30 @@ impl Handler<Tick> for Pong {
     type Result = ();
 
     fn handle(&mut self, _: Tick, ctx: &mut Self::Context) {
-        self.last_tick_time = Instant::now();
         self.tick(ctx);
     }
+}
+
+
+impl Handler<CountDown> for Pong {
+	type Result = ();
+
+	fn handle(&mut self, msg: CountDown, ctx: &mut Self::Context) {
+		self.paused = true;
+
+		let delay = 3;
+		self.send_to_players(Message(format!("Starting game in {} Seconds", delay)));
+
+		let ctx_addr = ctx.address();
+        let slept = actix::clock::sleep(Duration::from_secs(delay)).into_actor(self);
+        let fut = Box::pin(slept);
+        let fut = fut.then(move |_r, _, _| {
+            ctx_addr.do_send(GameStart);
+            actix::fut::ready(())
+        });
+
+        ctx.spawn(fut);
+	}
 }
 
 impl Handler<UpdateScore> for Pong {
@@ -121,18 +154,30 @@ impl Handler<UpdateScore> for Pong {
     fn handle(&mut self, msg: UpdateScore, ctx: &mut Self::Context) {
 		self.score[msg.side] += 1;
         self.send_to_players(Message(format!("SCORE {}:{}", self.score[0], self.score[1])));
-	
 
 		if self.score[msg.side] >= 3 {
-			ctx.address().do_send(GameOver);
+			ctx.notify(GameOver);
+		}
+		else {
+			ctx.notify(CountDown);
 		}
     }
 }
 
-// TODO maybe call it no connection, definitely need to stop this pong actor
+impl Handler<GameStart> for Pong {
+    type Result = ();
+
+    fn handle(&mut self, _: GameStart, ctx: &mut Self::Context) {
+        self.paused = false;
+        self.send_to_players(Message("BEG".to_owned()));
+		self.tick(ctx);
+    }
+}
+
 impl Handler<GameOver> for Pong {
     type Result = ();
 
+	// TODO maybe call GameOver with and Enum that determines why or who won
     fn handle(&mut self, _: GameOver, ctx: &mut Self::Context) {
         println!("GameOver");
 
