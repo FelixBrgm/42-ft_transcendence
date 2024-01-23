62d42cbb7f0fe6ca813770cb1fed4f734d0cc3cb
FelixBrgm <github.badly321@passinbox.com>
2024-01-23 14:32:16 +0100

    fixed dockerfile(outdated nodejs, rust cleanup, structure) added new client project, installed dependencies, problem with html socket - fixing it

--- client/src/main.js
+++ client/src/main.js
@@ -1,30 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import { createApp } from 'vue'
-import Vuex from 'vuex';
-import App from './App.vue'
-import VueNativeSock from 'vue-native-websocket';
+const app = createApp(App);
+app.use(router);
 
-createApp(App).mount('#app')
-
-export default new Vuex.Store({
-	state: {
-		accessToken: null,
-	},
-	mutations: {
-		setAccessToken(state, token) {
-			console.log('the accessToken is set now----------');
-			state.accessToken = token;
-		}
-	}
-})
-
-Vuex.use(VueNativeSock, 'http://127.0.0.1:8080/ws', {
-  format: 'json',
-  reconnection: true, // Enable automatic reconnection
-  reconnectionAttempts: 5, // Number of attempts
-  reconnectionDelay: 3000, // Delay between attempts in milliseconds
-});
-
-new Vuex({
-  render: (h) => h(App),
-}).$mount('#app');
+app.mount('#app');
