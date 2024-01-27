21b27f5ca693ea94cd91103b6186da6887eb4a8d
FelixBrgm <github.badly321@passinbox.com>
2024-01-27 17:13:38 +0100

    chat window only shows if logged in

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,36 @@
 <template>
-	<div id="app">
-		<ChatButton :showChat="showChat" @toggle-chat="toggleChat" />
-		<ChatWindow :showChat="showChat" @close-chat="closeChat" />
-		<router-view />
-	</div>
+  <div id="app">
+    <ChatButton v-if="showChatButton" :showChat="showChat" @toggle-chat="toggleChat" />
+    <ChatWindow :showChat="showChat" @close-chat="closeChat" />
+    <router-view/>
+  </div>
 </template>
 
 <script>
 import ChatButton from './components/elements/ChatButton.vue';
 import ChatWindow from './components/elements/ChatWindow.vue';
+import { auth } from './router'; // Import auth object from router.js
 
 export default {
-	components: {
-		ChatButton,
-		ChatWindow,
-	},
-	data() {
-		return {
-			showChat: false,
-		};
-	},
-	methods: {
-		toggleChat() {
-			this.showChat = !this.showChat;
-		},
-	},
+  components: {
+    ChatButton,
+    ChatWindow,
+  },
+  data() {
+    return {
+      showChat: false,
+    };
+  },
+  computed: {
+    showChatButton() { 
+      return auth.logged_in;
+    },
+  },
+  methods: {
+    toggleChat() {
+      this.showChat = !this.showChat;
+    },
+  },
 };
 </script>
 
@@ -33,7 +39,7 @@ export default {
 	min-height: 76vh;
 	display: flex;
 	flex-direction: column;
-	margin-bottom: 30px;
+	margin-bottom: 30px; 
 }
 
 @font-face {
