13066b66024594f6f339bd745a05efca37fae13a
Anna <annaselinafenzl@gmail.com>
2024-01-27 13:09:51 +0100

    functioning login

--- client/src/router.js
+++ client/src/router.js
@@ -9,29 +9,39 @@ import PongGame from './components/elements/PongGame.vue';
 import LoginPage from './components/pages/LoginPage';
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: HomePage, meta: { title: 'Transcendence' } },
-    { path: '/login', component: LoginPage },
-    { path: '/about', component: AboutUs, meta: { title: 'About Us' } },
-    { path: '/rules', component: RulesPage, meta: { title: 'Rules' } },
-    { path: '/dev', component: DevTest, meta: { title: 'Developer testing', backgroundColor: 'white' } },
-    { path: '/profile', component: ProfilePage, meta: { title: 'Profile', } },
-    { path: '/pong', component: PongGame, meta: { title: 'Pong', } },
-  ],
+	history: createWebHistory(),
+	routes: [
+		{ path: '/', component: HomePage, meta: { title: 'Transcendence' } },
+		{ path: '/login', component: LoginPage },
+		{ path: '/about', component: AboutUs, meta: { title: 'About Us' } },
+		{ path: '/rules', component: RulesPage, meta: { title: 'Rules' } },
+		{ path: '/dev', component: DevTest, meta: { title: 'Developer testing', backgroundColor: 'white' } },
+		{ path: '/profile', component: ProfilePage, meta: { title: 'Profile', } },
+		{ path: '/pong', component: PongGame, meta: { title: 'Pong', } },
+	],
 });
 
 router.beforeEach((to, from) => {
-  (from);
-  document.title = to.meta.title || 'Default Title';
-  if (to.meta.backgroundColor) { document.body.style.backgroundColor = to.meta.backgroundColor; }
-  else { document.body.style.backgroundColor = 'black'; }
+	(from);
+	document.title = to.meta.title || 'Default Title';
+	if (to.meta.backgroundColor) { document.body.style.backgroundColor = to.meta.backgroundColor; }
+	else { document.body.style.backgroundColor = 'black'; }
 
-  if (to.path !== "/login" && auth.user == null) {
-    return '/login';
-  }
+	console.log(auth);
+	console.log(to);
+	if (to.path === '/logged_in') {
+		auth.logged_in = true;
+		return '/';
+	}
+	if (to.path !== "/login" && auth.logged_in == false) {
+		return '/login';
+	}
+	if (to.path === "/login" && auth.logged_in) {
+		return true;
+	}
 
-  return true;
+
+	return true;
 });
 
 export default router;
@@ -41,5 +51,6 @@ export default router;
 import { reactive } from 'vue'
 
 export const auth = reactive({
-  user: null
+	user: null,
+	logged_in: false,
 })
