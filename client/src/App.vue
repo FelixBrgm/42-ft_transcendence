ccbbb1b4619a3c9ab735b03c9136bb7ae7269de9
Felix Brüggemann <gateway_painter_03@icloud.com>
2024-01-28 15:38:03 +0100

    Tournament fix

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,11 +2,13 @@
   <div id="app">
     <ChatButton v-if="showChatButton" :showChat="showChat" @toggle-chat="toggleChat" />
     <ChatWindow :showChat="showChat" @close-chat="closeChat" />
-    <router-view/>
+    <router-view />
+    <button @click="fetchData">TESTING</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import ChatButton from './components/elements/ChatButton.vue';
 import ChatWindow from './components/elements/ChatWindow.vue';
 import { auth } from './router'; // Import auth object from router.js
@@ -22,11 +24,25 @@ export default {
     };
   },
   computed: {
-    showChatButton() { 
+    showChatButton() {
       return auth.logged_in;
     },
   },
   methods: {
+    fetchData() {
+      console.log("HELLO");
+      // Make a GET request
+      axios.get('http://127.0.0.1:8080/auth/check')
+        .then(response => {
+          // Handle successful response
+          this.responseData = response.data;
+          console.log(this.responseData);
+        })
+        .catch(error => {
+          // Handle error
+          this.error = error.message;
+        });
+    },
     toggleChat() {
       this.showChat = !this.showChat;
     },
@@ -36,14 +52,14 @@ export default {
 
 <style>
 #Body {
-	min-height: 76vh;
-	display: flex;
-	flex-direction: column;
-	margin-bottom: 30px; 
+  min-height: 76vh;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
 }
 
 @font-face {
-	font-family: "neuropol";
-	src: url('assets/fonts/neuropol.ttf');
+  font-family: "neuropol";
+  src: url('assets/fonts/neuropol.ttf');
 }
 </style>
