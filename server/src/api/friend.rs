6fb17eb43fb590526f7899083c841b013a33f455
Anna <annaselinafenzl@gmail.com>
2024-02-22 00:42:08 +0100

    toggle enpoints

--- server/src/api/friend.rs
+++ server/src/api/friend.rs
@@ -3,7 +3,7 @@ use crate::db::Database;
 use actix_identity::Identity;
 use actix_web::{get, web, HttpResponse};
 
-#[get("/friend/toggle/{friend_id}")]
+#[get("/friend/{friend_id}")]
 async fn toggle(
     identity: Identity,
     db: web::Data<Database>,
@@ -22,46 +22,6 @@ async fn toggle(
 		false => { db.create_friendship(uid, friend_id)?; }
 		true => {db.remove_friendship(uid, friend_id)?; }
 	}
-
-    Ok(HttpResponse::Ok().finish())
-}
-
-#[get("/friend/add/{friend_id}")]
-async fn add(
-    identity: Identity,
-    db: web::Data<Database>,
-    friend: web::Path<i32>,
-) -> Result<HttpResponse, ApiError> {
-    let uid = identity.id()?.parse::<i32>()?;
-    let friend_id = friend.into_inner();
-
-    if !db.check_user(friend_id)? {
-        return Err(ApiError::BadRequest(
-            "Requested user doesn't exist".to_string(),
-        ));
-    }
-
-    db.create_friendship(uid, friend_id)?;
-
-    Ok(HttpResponse::Ok().finish())
-}
-
-#[get("/friend/remove/{friend_id}")]
-async fn remove(
-    identity: Identity,
-    db: web::Data<Database>,
-    friend: web::Path<i32>,
-) -> Result<HttpResponse, ApiError> {
-    let uid = identity.id()?.parse::<i32>()?;
-    let friend_id = friend.into_inner();
-
-    if !db.check_user(friend_id)? {
-        return Err(ApiError::BadRequest(
-            "Requested user doesn't exist".to_string(),
-        ));
-    }
-
-    db.remove_friendship(uid, friend_id)?;
 
     Ok(HttpResponse::Ok().finish())
 }
