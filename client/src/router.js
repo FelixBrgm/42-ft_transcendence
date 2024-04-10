891945d8469601eedd7208944cf6e3657c1c67df
FelixBrgm <github.badly321@passinbox.com>
2024-04-10 08:58:27 +0000

    adsf

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './components/pages/HomePage.vue';
 import PeoplePeople from './components/pages/PeoplePeople.vue';
 import RulesPage from './components/pages/RulesPage.vue';
 import ProfilePage from './components/pages/ProfilePage.vue';
-import PongGame from './components/pages/GamePage.vue'; 
-import LocalGame from './components/pages/LocalGame.vue'; 
-import AiGame from './components/pages/AiGame.vue'; 
+import PongGame from './components/pages/GamePage.vue';
+import LocalGame from './components/pages/LocalGame.vue';
+import AiGame from './components/pages/AiGame.vue';
 import LoginPage from './components/pages/LoginPage';
 import NamePage from './components/pages/NamePage';
 import NotFoundPage from './components/pages/NotFoundPage.vue'; // Import your custom 404 page
@@ -14,8 +14,8 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage, meta: { title: 'Transcendence' } },
-        { path: '/login', component: LoginPage, meta: { title: 'Login' } }, 
-        { path: '/name', component: NamePage, meta: { title: 'Login' } }, 
+        { path: '/login', component: LoginPage, meta: { title: 'Login' } },
+        { path: '/name', component: NamePage, meta: { title: 'Login' } },
         { path: '/people', component: PeoplePeople, meta: { title: 'PeoplePeople' } },
         { path: '/rules', component: RulesPage, meta: { title: 'Rules' } },
         { path: '/profile', component: ProfilePage, meta: { title: 'Profile', requiresAuth: true } }, // Example of a route that requires authentication
@@ -28,14 +28,14 @@ const router = createRouter({
 
 import store from './store';
 import axios from 'axios';
- 
+
 router.beforeEach(async (to, from, next) => {
 
     document.body.style.backgroundColor = to.meta.backgroundColor || 'black';
 
     if (!store.state.auth.user && to.path != "/login") {
         try {
-            const response = await axios.get('/api/user', {  
+            const response = await axios.get('/api/user', {
                 withCredentials: true,
             });
             store.commit('auth/setUser', response.data);
@@ -45,17 +45,9 @@ router.beforeEach(async (to, from, next) => {
     }
     if (to.path === "/login" && store.state.auth.user != null) {
         return next('/');
-    } 
+    }
     if (to.meta.requiresAuth && !store.state.auth.user) {
         return next('/login');
-    }
-    try {
-        const response = await axios.get('/api/user/alias/', {  withCredentials: true,});
-        if (response.data == false)
-            return next('/name');
-    } catch (error) {
-        console.log("error cathung uname");
-        console.log(error);
     }
     next();
 });
